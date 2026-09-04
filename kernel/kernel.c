88dd6786b716ed7a7561ea60b47dc23cd579e4a9
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-09-04 19:15:16 +0100

    Call printf function in kernel_main

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,4 +1,5 @@
 #include "kernel.h";
+#include "../common/common.h"
 typedef unsigned char uint8_t;
 typedef unsigned int uint32_t;
 typedef uint32_t size_t;
@@ -37,10 +38,13 @@ void *memset(void *buf, char c, size_t n) {
 
 void kernel_main(void) {
     //memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
-    const char *s = "\n\nKernel Running...\n";
-    for (int i = 0; s[i] != '\0'; i++) {
-        putchar(s[i]);
-    }
+    //const char *s = "\n\nKernel Running...\n";
+    //for (int i = 0; s[i] != '\0'; i++) {
+    //    putchar(s[i]);
+    //}
+
+    printf("\n\nHello %s\n", "World!");
+    printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
     for (;;) {
         __asm__ __volatile__("wfi");
     }
