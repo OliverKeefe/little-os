2d03c3f2d5abe00cb57c44bd0908d54ffb30a5fb
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-09-04 19:17:56 +0100

    Implement printf, memcpy, memset and strcpy functions
    * Add memcpy for copying memory between buffers.
    * Add memset for filling memory regions with a value.
    * Add strcpy for null-terminated string copying.
    * Move memset implementation from kernel.c to common.c.

---
+++ common/common.c
@@ -0,0 +1,91 @@
+#include "common.h"
+
+void putchar(char char_binary);
+
+void printf(const char *fmt, ...) {
+    va_list vargs;
+    va_start(vargs, fmt);
+
+    while (*fmt) {
+        if (*fmt == '%') {
+            fmt++; // Skips '%' format specifier.
+
+            switch (*fmt) {
+                case '\0':
+                    putchar('%');
+                    goto end;
+                case '%':
+                    putchar('%');
+                    break;
+                case 's': {
+                    const char *s = va_arg(vargs, const char *);
+                    while (*s) {
+                        putchar(*s);
+                        s++;
+                    }
+                    break;
+                }
+                case 'd': {
+                    int value = va_arg(vargs, int);
+                    unsigned magnitude = value;
+                    if (value < 0) {
+                        putchar('-');
+                        magnitude = -magnitude;
+                    }
+
+                    unsigned divisor = 1;
+                    while (magnitude / divisor > 9)
+                        divisor *= 10;
+
+                    while (divisor > 0) {
+                        putchar('0' + magnitude / divisor);
+                        magnitude %= divisor;
+                        divisor /= 10;
+                    }
+
+                    break;
+                }
+                case 'x': {
+                    unsigned value = va_arg(vargs, unsigned);
+                    for (int i = 7; i >= 0; i--) {
+                        unsigned nibble = (value >> (i * 4)) & 0xf;
+                        putchar("0123456789abcdef"[nibble]);
+                    }
+                }
+            }
+        } else {
+            putchar(*fmt);
+        }
+
+        fmt++;
+    }
+
+    end:
+        va_end(vargs);
+}
+
+void *memcpy(void *dst, const void *src, size_t n) {
+    uint8_t *d = (uint_8_t *) dst;
+
+    const uint8_t *s = (const uint8_t *) src;
+
+    while (n--)
+        *d++ = *s++;
+    return dst;
+}
+
+void *memset(void *buf, char c, size_t n) {
+    uint8_t *p = (uint8_t) buf;
+
+    while (n--)
+        *p++ = c;
+    return buf;
+}
+
+char *strcpy(char *dst, const char *src) {
+    char *d = dst;
+    while (*src)
+        *d++ = *src++;
+    *d = '\0';
+    return dst;
+}
