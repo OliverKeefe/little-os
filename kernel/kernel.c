e50c69c696a6b1a510e9b20cd39c04080fe01300
OliverKeefe <104454089+oliverkeefe@users.noreply.github.com>
2026-09-08 18:33:23 +0100

    Add kernel panic macro

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -37,7 +37,9 @@ void kernel_main(void) {
     //}
 
     printf("\n\nHello %s\n", "World!");
+    PANIC("test panic");
     printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
+
     for (;;) {
         __asm__ __volatile__("wfi");
     }
