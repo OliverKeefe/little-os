9a90b14c2cc07e2850dbf7c0a85cde9e78f2f7b2
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-09-19 12:13:50 +0100

    Add alloc_pages function
    Handles allocation of memory to physical addresses.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -37,6 +37,31 @@ void handle_trap(struct trap_frame *f) {
     PANIC("unexpected trap scause=%x, stval=%x, sepc=%x\n", scause, stval, user_pc);
 }
 
+
+
+extern char __free_ram[], __free_ram_end[];
+
+/*
+ alloc_pages handles the allocation of memory to physical addresses.
+ Note, currently this uses a bump allocator but long term a different
+ algorithm must be used because currently, memory can't actually be
+ freed after allocation.
+
+ Additionally, deallocation should be implemented using a bitmap-based
+ algorithm or buddy system.
+ */
+paddr_t alloc_pages(uint32_t n) {
+    static paddr_t next_paddr = (paddr_t) __free_ram;
+    paddr_t paddr = next_paddr;
+    next_paddr += n * PAGE_SIZE;
+
+    if (next_paddr > (paddr_t) __free_ram_end)
+        PANIC("out of memory");
+
+    memset((void *) paddr, 0, n * PAGE_SIZE);
+    return paddr;
+}
+
 __attribute__((naked))
 __attribute__((aligned(4)))
 void kernel_entry(void) {
@@ -122,13 +147,16 @@ void kernel_main(void) {
     //    putchar(s[i]);
     //}
 
-    printf("\n\nHello %s\n", "World!");
-    PANIC("test panic");
-    printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
+    paddr_t paddr0 = alloc_pages(2);
+    paddr_t paddr1 = alloc_pages(1);
+    printf("alloc_pages test: paddr0=%x\n", paddr0);
+    printf("alloc_pages test: paddr1=%x\n", paddr1);
 
-    for (;;) {
-        __asm__ __volatile__("wfi");
-    }
+    PANIC("booted!");
+
+    //for (;;) {
+    //    __asm__ __volatile__("wfi");
+    //}
 }
 
 /*
